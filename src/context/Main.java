6aabc65f4748a265d0a1d620c3c3eafed1dce33d
Juan Manuel Eraso <juaneraso2003@hotmail.com>
2026-06-14 15:54:11 -0500

    Septimo ejercicio APOII reorganizado

---
+++ src/context/Main.java
@@ -0,0 +1,19 @@
+package context;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Atributos
+		int pg = 5;
+        int pp = 3;
+        int pe = 2;
+        
+        //Formula
+        int puntaje=(pg * 3) + (pp * 0) + (pe * 1);
+        
+        //Resultado
+        System.out.print("Puntaje: "+puntaje);
+
+	}
+
+}
